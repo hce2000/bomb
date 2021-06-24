27434da6b580c9a2818348e60801550006fab82c
徐晨恩 <love.1223604@gmail.com>
2021-06-24 15:22:59 +0800

    3st commit

--- app/src/main/java/tw/edu/pu/csim/s1081640/bomb/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1081640/bomb/MainActivity.kt
@@ -1,11 +1,19 @@
 package tw.edu.pu.csim.s1081640.bomb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        start_button.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(p0: View?) {
+                intent = Intent(this@MainActivity, 要跳轉到的頁面名稱::class.java)
+                startActivity(intent)
+            }
+        })
     }
 }
